27b0ae4217d20cec86cc55fc70fca9f85aff3a9d
Mario Tormo <mario@tormo-romero.eu>
2021-10-20 17:01:49 +0200

    Create Ejercicio_I_1.cpp

---
+++ fp/Ejercicio_I_1.cpp
@@ -0,0 +1,38 @@
+/**************************************\
++                                      +
++   Programa Ejercicio_I_1             +
++   (Ver. 1.2)                         +
++                                      +
++   Imprime la tabla de multiplicar    +
++   por un número leído como dato      +
++                                      +
+\**************************************/
+#include <stdio.h>
+
+int main() {
+  /* Constantes del programa */
+  const int minimo = 1;  /* Valor mínimo para la tabla */
+  const int maximo = 10; /* Valor máximo para la tabla */
+
+  /* Declaración de variables */
+  int numero = -1; /* Número a leer inicializado a -1 */
+
+  /* Leer el número */ {
+    do {
+      printf("Numero? ");
+      fflush(stdin);
+      scanf("%d", &numero);
+    } while ((numero < minimo) || (numero > maximo));
+  }
+
+  /* Imprimir la cabecera */ {
+    printf("\nTabla de multiplicar por %d\n", numero);
+    printf("===========================\n");
+  }
+
+  /* Imprimir la tabla de multiplicar */ {
+    for (int i = minimo; i <= maximo; i++) {
+      printf("%6d  x %2d = %3d\n", numero, i, numero*i);
+    }
+  }
+}
